10e9dbd278e8b56b039a1477d50805856a8b3011
Vitor Nascimento <vitor.nascimento@arizona.com.br>
2014-02-04 11:50:18 +0000

    Change binary strategy to ark cookbook, now working in redhat

--- attributes/default.rb
+++ attributes/default.rb
@@ -24,11 +24,11 @@ case node['platform_family']
     default['nodejs']['install_method'] = 'source'
 end
 
-default['nodejs']['version'] = '0.10.15'
-default['nodejs']['checksum'] = '87345ab3b96aa02c5250d7b5ae1d80e620e8ae2a7f509f7fa18c4aaa340953e8'
-default['nodejs']['checksum_linux_x64'] = '0b5191748a91b1c49947fef6b143f3e5e5633c9381a31aaa467e7c80efafb6e9'
-default['nodejs']['checksum_linux_x86'] = '7ff9fb6aa19a5269a5a2f7a770040b8cd3c3b528a9c7c07da5da31c0d6dfde4d'
-default['nodejs']['dir'] = '/usr/local'
+default['nodejs']['version'] = '0.10.25'
+default['nodejs']['checksum'] = '46eef3b9d5475a2081dc2b2f7cf1f4c3a56824d1fc9b04e7ed1d7a88e8f6b36f'
+default['nodejs']['checksum_linux_x64'] = '1dac61c21fa21e47fc6e799757569c6c3914897ca46fc8f4dd2c8f13f0400626'
+default['nodejs']['checksum_linux_x86'] = '3f1fa0bb332b1354bca8d52d89e92c8884b6469a0f5fc3826ee72c2639279995'
+default['nodejs']['dir'] = '/usr/local/nodejs'
 default['nodejs']['npm'] = '1.3.5'
 default['nodejs']['src_url'] = "http://nodejs.org/dist"
 default['nodejs']['make_threads'] = node['cpu'] ? node['cpu']['total'].to_i : 2
--- recipes/install_from_binary.rb
+++ recipes/install_from_binary.rb
@@ -16,6 +16,7 @@
 # limitations under the License.
 #
 
+include_recipe "ark"
 
 # Shamelessly borrowed from http://docs.opscode.com/dsl_recipe_method_platform.html
 # Surely there's a more canonical way to get arch?
@@ -40,41 +41,13 @@ end
 # Let the user override the source url in the attributes
 nodejs_bin_url = "#{node['nodejs']['src_url']}/#{nodejs_tar_path}"
 
-# Download it:
-remote_file "/usr/local/src/#{nodejs_tar}" do
-  source nodejs_bin_url
-  checksum expected_checksum
-  mode 0644
-  action :create_if_missing
+# Install NodeJs
+ark 'nodejs' do
+  url             nodejs_bin_url
+  checksum        expected_checksum
+  path            node['nodejs']['dir']
+  home_dir        node['nodejs']['dir']
+  version         node['nodejs']['version']
+  append_env_path true
 end
 
-# Where we will install the binaries and libs to (normally /usr/local):
-destination_dir = node['nodejs']['dir']
-
-install_not_needed = File.exists?("#{node['nodejs']['dir']}/bin/node") && `#{node['nodejs']['dir']}/bin/node --version`.chomp == "v#{node['nodejs']['version']}" 
-
-# Verify the SHA sum of the downloaded file:
-ruby_block "verify_sha_sum" do
-    block do
-        require 'digest/sha1'
-        calculated_sha256_hash = Digest::SHA256.file("/usr/local/src/#{nodejs_tar}")
-        if calculated_sha256_hash != expected_checksum
-            raise "SHA256 Hash of #{nodejs_tar} did not match!  Expected #{expected_checksum} found #{calculated_sha256_hash}"
-        end
-    end
-    not_if { !node['nodejs']['check_sha'] or install_not_needed }
-end
-
-# One hopes that we can trust the contents of the node tarball not to overwrite anything it shouldn't!
-execute "install package to system" do
-    command <<-EOF
-            tar xf /usr/local/src/#{nodejs_tar} \
-            --strip-components=1  --no-same-owner \
-            -C #{destination_dir} \
-            #{package_stub}/bin \
-            #{package_stub}/lib \
-            #{package_stub}/share
-        EOF
-
-    not_if { install_not_needed }
-end
